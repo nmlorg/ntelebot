c82caf05adae70d08d19996eb68f843cac821063
Daniel Reed <nmlorg@gmail.com>
2018-08-08 21:34:32 -0700

    Let's drop the "py" prefix from the project, just to keep it synced to the package and module name.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     author='Daniel Reed',
     author_email='nmlorg@gmail.com',
     description='A simple implementation of https://core.telegram.org/bots/api.',
-    url='https://github.com/nmlorg/pyntelebot',
+    url='https://github.com/nmlorg/ntelebot',
     packages=['ntelebot'],
     install_requires=['requests'])
