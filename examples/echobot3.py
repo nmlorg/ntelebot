ff54a6436b1adbe0322fe54d244e3edb80850846
Daniel Reed <nmlorg@gmail.com>
2018-08-10 13:46:06 -0700

    Quick example showing how to use ntelebot.preprocess.Preprocessor.

---
+++ examples/echobot3.py
@@ -0,0 +1,44 @@
+"""A version of echobot2.py that uses ntelebot.preprocess.Preprocessor."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pprint
+import sys
+
+import ntelebot
+
+
+class Dispatcher(object):  # pylint: disable=too-few-public-methods
+    """Print all updates and echo back all messages."""
+
+    def __init__(self):
+        self.preprocessor = ntelebot.preprocess.Preprocessor()
+
+    def __call__(self, bot, update):
+        ctx = self.preprocessor(bot, update)
+        if ctx:
+            pprint.pprint(ctx.__dict__)
+            ctx.reply_text('\U0001f50a ' + ctx.text)
+
+
+def main(argv):  # pylint: disable=missing-docstring
+    if len(argv) < 2:
+        print('Usage: %s BOT:TOKEN [BOT:TOKEN [BOT:TOKEN [...]]]' % argv[0])
+        print()
+        print('Where BOT:TOKEN is one or more codes received from following the process at '
+              'https://core.telegram.org/bots#creating-a-new-bot.')
+        return 1
+
+    loop = ntelebot.loop.Loop()
+    dispatcher = Dispatcher()
+
+    for token in argv[1:]:
+        bot = ntelebot.bot.Bot(token)
+        pprint.pprint(bot.get_me())
+        loop.add(bot, dispatcher)
+
+    loop.run()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
