37932e15dfd5c86835bbb53cd70aeeff5ccb25fd
Daniel Reed <nmlorg@gmail.com>
2018-08-24 21:03:47 -0700

    Woops, I didn't realize this was a literal list of packages.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setuptools.setup(
     name='ntelebot',
-    version='0.0.1dev',
+    version='0.0.1.dev0',
     author='Daniel Reed',
     author_email='nmlorg@gmail.com',
     description='A simple implementation of https://core.telegram.org/bots/api.',
     url='https://github.com/nmlorg/ntelebot',
-    packages=['ntelebot'],
+    packages=setuptools.find_packages(include=('ntelebot', 'ntelebot.*')),
     install_requires=['requests'])
