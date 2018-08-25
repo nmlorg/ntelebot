145bee536fd628d60782a780f0011762640975fb
Daniel Reed <nmlorg@gmail.com>
2018-08-25 16:27:42 -0700

    Add very basic token validation.

--- ntelebot/test_bot.py
+++ ntelebot/test_bot.py
@@ -10,24 +10,24 @@ import ntelebot
 def test_token():
     """Verify the token is used correctly to construct the base URL."""
 
-    assert ntelebot.bot.Bot('test:test').url == 'https://api.telegram.org/bottest:test/'
+    assert ntelebot.bot.Bot('1234:test').url == 'https://api.telegram.org/bot1234:test/'
+
+    with pytest.raises(AssertionError):
+        ntelebot.bot.Bot('test:test')
+    with pytest.raises(AssertionError):
+        ntelebot.bot.Bot('1234:test/getme?')
 
 
 def test_getattr_magic():
     """Verify Bot().methodName returns a _Request instance with a normalized URL."""
 
     # pylint: disable=protected-access
-    assert isinstance(ntelebot.bot.Bot('test:test').getMe, ntelebot.bot._Request)
-    assert ntelebot.bot.Bot('test:test').get_me.url == 'https://api.telegram.org/bottest:test/getme'
+    assert isinstance(ntelebot.bot.Bot('1234:test').getMe, ntelebot.bot._Request)
+    assert ntelebot.bot.Bot('1234:test').get_me.url == 'https://api.telegram.org/bot1234:test/getme'
 
 
 def test_request():
     """Verify simple live echo requests return the appropriate response."""
-
-    bot = ntelebot.bot.Bot('test:test')
-    bot.get_me.respond(real_http=True)
-    with pytest.raises(ntelebot.errors.NotFound):
-        bot.get_me()
 
     # From https://core.telegram.org/bots/api#authorizing-your-bot.
     bot = ntelebot.bot.Bot('123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
@@ -38,6 +38,10 @@ def test_request():
     # From https://github.com/python-telegram-bot/python-telegram-bot/blob/master/tests/bots.py.
 
     bot = ntelebot.bot.Bot('133505823:AAHZFMHno3mzVLErU5b5jJvaeG--qUyLyG0')
+    bot.get_dummy.respond(real_http=True)
+    with pytest.raises(ntelebot.errors.NotFound):
+        bot.get_dummy()
+
     bot.get_me.respond(real_http=True)
     assert bot.get_me() == {
         'first_name': 'PythonTelegramBot',
