9eeb5b83478234c541a10c5e7408c7e6bea36dfe
Daniel Reed <nmlorg@gmail.com>
2021-05-07 19:24:55 -0700

    Switch from https://python-telegram-bot.org/'s [defunct] test bot to one I just created for ntelebot.
    Also address a long-standing issue where the test fails if any updates haven't been acknowledged. Doing so is a destructive process—if the test is run against a bot that's actually in use, updates generated after the actual bot's last fetch will be lost.

--- ntelebot/test_bot.py
+++ ntelebot/test_bot.py
@@ -4,6 +4,8 @@ import pytest
 
 import ntelebot
 
+TEST_BOT_TOKEN = '1824972844:AAFKcZlNoyGnA2rkdemlHCrJ5432fXYm9dE'
+
 
 def test_token():
     """Verify the token is used correctly to construct the base URL."""
@@ -35,20 +37,27 @@ def test_request():
 
     # From https://github.com/python-telegram-bot/python-telegram-bot/blob/master/tests/bots.py.
 
-    bot = ntelebot.bot.Bot('133505823:AAHZFMHno3mzVLErU5b5jJvaeG--qUyLyG0')
+    bot = ntelebot.bot.Bot(TEST_BOT_TOKEN)
     bot.get_dummy.respond(real_http=True)
     with pytest.raises(ntelebot.errors.NotFound):
         bot.get_dummy()
 
     bot.get_me.respond(real_http=True)
     assert bot.get_me() == {
-        'first_name': 'PythonTelegramBot',
-        'id': 133505823,
+        'can_join_groups': False,
+        'can_read_all_group_messages': False,
+        'first_name': 'ntelebot',
+        'id': int(TEST_BOT_TOKEN.split(':', 1)[0]),
         'is_bot': True,
-        'username': 'PythonTelegramBot',
+        'supports_inline_queries': False,
+        'username': 'ntelebot',
     }
 
-    bot = ntelebot.bot.Bot('133505823:AAHZFMHno3mzVLErU5b5jJvaeG--qUyLyG0', timeout=2)
+    bot = ntelebot.bot.Bot(TEST_BOT_TOKEN, timeout=2)
     bot.get_updates.respond(real_http=True)
+    offset = None
+    updates = bot.get_updates(timeout=0)
+    if updates:
+        offset = updates[-1]['update_id'] + 1
     with pytest.raises(ntelebot.errors.Timeout):
-        bot.get_updates(timeout=3)
+        bot.get_updates(offset=offset, timeout=3)
