ab03a5cc76b2b7d3e0e542069b8b6bfe373fa7f0
Daniel Reed <nmlorg@gmail.com>
2026-01-03 15:38:37 -0800

    Enable TCP keepalives on all outgoing connections.
    Since 0045680, every thread (typically the main thread plus 1 per bot) attempts to maintain a constant connection to api.telegram.org. Per-bot threads try to ensure there is activity on their connection at least once every 10 seconds (if there is no activity for 12 seconds, the connection is broken), but the main thread is generally just used for responses. (In metabot, it is also woken up once an hour to update group chat admin lists.) If api.telegram.org ends a connection in the interim, urllib3 should be able to detect that (by calling select() on the socket immediately before writing to it), but in practice sometimes (multiple times a day) the disconnect isn't detected until after a request has been sent; presumably because api.telegram.org either did not send a connection-termination packet, or the packet was consumed along the way. The main intent of enabling keepalives is to simply provoke api.telegram.org into forcibly [re]terminating connections that it [thinks it] has already closed.
    
    See #9.
    
    Release as 0.4.5.1.

--- ntelebot/requests.py
+++ ntelebot/requests.py
@@ -1,9 +1,11 @@
 """A wrapper around requests.api.* that reuses a single Session instance per thread."""
 
+import socket
 import threading
 
 import requests
 from requests.exceptions import *  # pylint: disable=redefined-builtin,unused-wildcard-import,wildcard-import
+import urllib3
 
 
 class _Local(threading.local):  # pylint: disable=too-few-public-methods
@@ -11,6 +13,23 @@ class _Local(threading.local):  # pylint: disable=too-few-public-methods
     def __init__(self):
         super().__init__()
         self.session = requests.Session()
+        adapter = _KeepAliveAdapter()
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
+
+
+class _KeepAliveAdapter(requests.adapters.HTTPAdapter):
+
+    def init_poolmanager(self, *args, **kwargs):
+        assert 'socket_options' not in kwargs
+        # See https://github.com/nmlorg/ntelebot/issues/9#issuecomment-2302631974.
+        kwargs['socket_options'] = urllib3.connection.HTTPConnection.default_socket_options + [
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 115),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 30),
+            (socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 4),
+        ]
+        return super().init_poolmanager(*args, **kwargs)
 
 
 _LOCAL = _Local()
